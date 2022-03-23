2cde085605e2bc56f12c1f507ef352ff384346e8
Charles Sy <charles.m.sy@hotmail.com>
2022-03-23 15:21:04 -0700

    account creation fragment XML Created

--- TextTrader/app/src/main/java/com/example/texttrader/MainActivity.java
+++ TextTrader/app/src/main/java/com/example/texttrader/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment_landing);
+        setContentView(R.layout.landing_fragment);
     }
 }
