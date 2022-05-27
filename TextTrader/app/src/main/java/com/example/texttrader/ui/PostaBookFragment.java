dfd94172c44613d52dd0ec4e754e99ed2b585962
Charles Sy <charles.m.sy@hotmail.com>
2022-05-27 16:19:31 -0700

    Slight update of XML on book post screen

--- TextTrader/app/src/main/java/com/example/texttrader/ui/PostaBookFragment.java
+++ TextTrader/app/src/main/java/com/example/texttrader/ui/PostaBookFragment.java
@@ -4,6 +4,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -11,15 +14,40 @@ import androidx.fragment.app.Fragment;
 
 import com.example.texttrader.R;
 
-public class PostaBookFragment extends Fragment {
+public class PostaBookFragment extends Fragment implements AdapterView.OnItemSelectedListener {
+
+    private Spinner bookPostSpinner;
+    public String sselectedSpinnerText;
 
     @Override
     public View onCreateView(
             @NonNull LayoutInflater inflater, ViewGroup container, Bundle saveInstanceState) {
         View view = inflater.inflate(R.layout.book_post_form_fragment, container, false);
 
+        bookPostSpinner = view.findViewById(R.id.book_post_spinner);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getActivity(), R.array.trade_or_donate, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
+        bookPostSpinner.setAdapter(adapter);
+        bookPostSpinner.setOnItemSelectedListener(this);
+
         Toast.makeText(getActivity(), "A book post form should pop up!!", Toast.LENGTH_SHORT).show();
 
         return view;
+    };
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        String choice = adapterView.getItemAtPosition(i).toString();
+        sselectedSpinnerText = choice;
+        if (i > 0) {
+            Toast.makeText(getActivity(), choice + " selcted.", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
     }
 }
