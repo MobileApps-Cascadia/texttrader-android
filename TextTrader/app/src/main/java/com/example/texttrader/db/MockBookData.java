349011c40117dc848a45019c6f14180e55df2ec6
Charles Sy <charles.m.sy@hotmail.com>
2022-06-09 15:31:26 -0700

    book post form can post into mock book data list and booklist item update

--- TextTrader/app/src/main/java/com/example/texttrader/db/MockBookData.java
+++ TextTrader/app/src/main/java/com/example/texttrader/db/MockBookData.java
@@ -16,10 +16,10 @@ public class MockBookData {
         booksList.add(new Book(R.drawable.the_zombie_survival_guide,"The Zombie Survival Guide","1st","Max Brooks" ,"Donate"));
         booksList.add(new Book(R.drawable.world_war_z,"World War Z","1st","Max Brooks" ,"Donate"));
         booksList.add(new Book(R.drawable.the_art_of_war,"The Art of War","100th","Sun Tzu","Trade"));
+        booksList.add(new Book(R.drawable.everyday_calculus,"Everyday Calculus","1st","Oscar E. Fernandez","Trade"));
+        booksList.add(new Book("This Book Loves You","1st","Pewdiepie","Trade"));
         booksList.add(new Book(R.drawable.basic_physics,"Basic Physics","3rd","Karl F. Kuhn" ,"Trade"));
         booksList.add(new Book(R.drawable.the_forever_war,"Thh Forever War","1st","Joe Haldeman" ,"Donate"));
-        booksList.add(new Book(R.drawable.everyday_calculus,"Everyday Calculus","1st","Oscar E. Fernandez","Trade"));
-        booksList.add(new Book("This Book Loves You","1st","Pewds","Trade"));
     }
 
     public static void addNewBook(String s, String title, String edition, String authorName, String donationStatus) {
